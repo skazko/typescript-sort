bd77aa0830d6fc3ec04d7ef24f14e78980cf7f97
Slava <kreeon@ya.ru>
2020-04-13 15:33:40 +0300

    refactor with abstract class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, -3, 8, 99, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('Xaaasd');
-const s = new Sorter(charactersCollection);
-s.sort();
+
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
@@ -18,6 +16,6 @@ linkedList.add(500);
 linkedList.add(-2);
 linkedList.add(43);
 
-const ss = new Sorter(linkedList);
-ss.sort();
+// const ss = new Sorter(linkedList);
+linkedList.sort();
 linkedList.print();
