6ee9abceb6a060a3d855e92be85f1e6d40a2517d
Slava <kreeon@ya.ru>
2020-04-13 15:06:34 +0300

    first implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, -3, 8, 99, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaaasd');
+const s = new Sorter(charactersCollection);
+s.sort();
+console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-2);
+linkedList.add(43);
+
+const ss = new Sorter(linkedList);
+ss.sort();
+linkedList.print();
